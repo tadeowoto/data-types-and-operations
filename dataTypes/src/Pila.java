963144167ff0140aae78779f0e3d0b6b113ef67b
florenciacastro95 <fmcastro95@gmail.com>
2024-04-04 17:21:42 -0300

    Hice clase Pila y su constructor

---
+++ dataTypes/src/Pila.java
@@ -0,0 +1,16 @@
+
+public class Pila {
+ 
+    private int[] pilita;
+    private int tope;
+    private int tam;
+
+    public Pila(int tam) {
+        this.tam = tam;
+        pilita= new int[tam];
+        tope=-1;
+    }
+    
+    
+   
+}
